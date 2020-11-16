52362190bb028a3dc5f7c091845e63a37edf0bdf
Shahar s <cl_shaharshavit@rnd-hub.com>
2020-11-16 15:40:49 +0200

    chore(cr): updated for code review

--- tests/fsOperationsTests.js
+++ tests/fsOperationsTests.js
@@ -5,6 +5,7 @@ const logger = container.get('logger');
 const chai = require('chai');
 const chaiAsPromised = require('chai-as-promised');
 const sinon = require('sinon');
+const path = require('path');
 const expect = chai.expect;
 
 chai.use(chaiAsPromised);
@@ -23,27 +24,31 @@ describe('filesystem operations test', function () {
   });
 
   it('create folders of non existing path when trying to write', function () {
-    return expect(fsHandler.writeFile('file://D-P-ZIVDA/myShared/csms/testfile.json', 'Some test content for file')).to.eventually.be.fulfilled;
+    const filePath = path.resolve(__dirname, 'data', 'testfile.json');
+    return expect(fsHandler.writeFile(`file://${filePath}`, 'Some test content for file')).to.eventually.be.fulfilled;
   });
 
   it('file exists', function (done) {
-    return fsHandler.exists('file://D-P-ZIVDA/myShared/csms/testfile.json').then(exists => {
+    const filePath = path.resolve(__dirname, 'data', 'testfile.json');
+    fsHandler.exists(`file://${filePath}`).then(exists => {
       expect(exists).to.equal(true);
       done();
     });
   });
 
   it('file not exists', function (done) {
-    return fsHandler.exists('file://D-P-ZIVDA/myShared/csms/testfile0.json').then(exists => {
+    const filePath = path.resolve(__dirname, 'data', 'testfile0.json');
+    fsHandler.exists(`file://${filePath}`).then(exists => {
       expect(exists).to.equal(false);
       done();
     });
   });
 
   it('read from directories', function (done) {
-    return fsHandler.readDir('file://D-P-ZIVDA/myShared/csms').then(result => {
+    const filePath = path.resolve(__dirname, 'data');
+    fsHandler.readDir(`file://${filePath}`).then(result => {
       expect(result.length).to.equal(1);
       done();
-    });
+    }).catch(err => console.log(err));
   });
 });
